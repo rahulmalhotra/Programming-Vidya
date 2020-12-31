ff3d1b391e25d1923f46847c2b9d2172047417ed
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2020-12-31 22:14:14 +0530

    Added solution for HackerRank Print the Elements of a Linked List problem

---
+++ Hackerrank/Data Structures/Easy/print-the-elements-of-a-linked-list.cpp
@@ -0,0 +1,106 @@
+/*
+*   Author:- Rahul Malhotra
+*   Source:- Programming Vidya
+*   Description:- Solution for HackerRank Print the Elements of a Linked List problem
+*   Problem Link:- https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
+*   Website:- www.programmingvidya.com
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+
+    // * Loop while head pointer is not equal to NULL
+    while(head!=NULL) {
+
+        /*
+        *   Display the data of the current node
+        *   to which the head pointer is pointing
+        */
+        cout<<head->data<<endl;
+
+        /*
+        *   Move the head pointer to the next node
+        *   of the linked list
+        */
+        head = head->next;
+    }
+}
+
+// * Main function as given by HackerRank
+int main()
+{
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    printLinkedList(llist->head);
+
+    return 0;
+}
