ab985caf1e33b1320bc4ccbfeb071a57e68fca5a
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2021-01-06 09:00:59 +0530

    Added solution for HackerRank Print in Reverse problem

---
+++ Hackerrank/Data Structures/Easy/print-the-elements-of-a-linked-list-in-reverse.cpp
@@ -0,0 +1,117 @@
+/*
+*   Author:- Rahul Malhotra
+*   Source:- Programming Vidya
+*   Description:- Solution for HackerRank Print in Reverse problem
+*   Problem Link:- https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse/problem
+*   Website:- www.programmingvidya.com
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep) {
+    while (node) {
+        cout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            cout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reversePrint function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void reversePrint(SinglyLinkedListNode* head) {
+
+    // * If head is not NULL
+    if(head!=NULL) {
+
+        // * Call the reversePrint() with the next node
+        reversePrint(head->next);
+
+        // * Print the data of current node i.e. head
+        cout<<head->data<<endl;
+    }
+}
+
+// * Main function as given by HackerRank
+int main()
+{
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        reversePrint(llist->head);
+    }
+
+    return 0;
+}
