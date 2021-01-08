bf3b5cd30022401b2124881450cbc4a05f3130d7
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2021-01-08 11:52:13 +0530

    Added solution for HackerRank Compare two linked lists problem

---
+++ Hackerrank/Data Structures/Easy/compare-two-linked-lists.cpp
@@ -0,0 +1,159 @@
+/*
+*   Author:- Rahul Malhotra
+*   Source:- Programming Vidya
+*   Description:- Solution for HackerRank Compare two linked lists problem
+*   Problem Link:- https://www.hackerrank.com/challenges/compare-two-linked-lists/problem
+*   Website:- www.programmingvidya.com
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the compare_lists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+
+    // * Looping while both the pointers are not NULL
+    while(head1!=NULL && head2!=NULL) {
+
+        /*
+        *   If at any index,
+        *   the data value of both the nodes
+        *   are not equal, return false.
+        *   As the linked lists are not equal
+        */
+        if(head1->data != head2->data) {
+            return false;
+        }
+
+        // * Move head of first linked list to next node
+        head1 = head1->next;
+
+        // * Move head of second linked list to next node
+        head2 = head2->next;
+    }
+
+    /*
+    *   If any of the linked list has not
+    *   reached the end, return false as
+    *   the linked lists are not equal
+    */
+    if(head1!=NULL || head2!=NULL) {
+        return false;
+    }
+
+    // * Return true as the linked lists are equal
+    return true;
+}
+
+// * Main function as given by HackerRank
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        SinglyLinkedList* llist1 = new SinglyLinkedList();
+
+        int llist1_count;
+        cin >> llist1_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist1_count; i++) {
+            int llist1_item;
+            cin >> llist1_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist1->insert_node(llist1_item);
+        }
+
+      	SinglyLinkedList* llist2 = new SinglyLinkedList();
+
+        int llist2_count;
+        cin >> llist2_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist2_count; i++) {
+            int llist2_item;
+            cin >> llist2_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist2->insert_node(llist2_item);
+        }
+
+        bool result = compare_lists(llist1->head, llist2->head);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
