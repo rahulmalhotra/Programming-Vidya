013d1c66ede3d992f6871858424b82a0f7866784
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2021-01-18 09:42:13 +0530

    Added solution for HackerRank Tree: Postorder Traversal problem

---
+++ Hackerrank/Data Structures/Easy/tree-postorder-traversal.cpp
@@ -0,0 +1,99 @@
+/*
+*   Author:- Rahul Malhotra
+*   Source:- Programming Vidya
+*   Description:- Solution for HackerRank Tree: Postorder Traversal problem
+*   Problem Link:- https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+*   Website:- www.programmingvidya.com
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.
+Node is defined as
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+    void postOrder(Node *root) {
+
+        // * If root is NULL, return
+        if(root==NULL) {
+            return;
+        }
+
+        // * Print the left subtree
+        preOrder(root->left);
+
+        // * Print the right subtree
+        preOrder(root->right);
+
+        // * Print the root node
+        cout<<root->data<<" ";
+
+    }
+
+}; //End of Solution
+
+int main() {
+
+    Solution myTree;
+    Node* root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+	myTree.postOrder(root);
+    return 0;
+}
