73f58e5d09ec16bf04849e175a91594d1aecf15e
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2021-01-19 09:43:26 +0530

    Updated solution for HackerRank Down to Zero II problem

--- Hackerrank/Data Structures/Medium/down-to-zero-ii.cpp
+++ Hackerrank/Data Structures/Medium/down-to-zero-ii.cpp
@@ -3,66 +3,93 @@
 *   Source:- Programming Vidya
 *   Description:- Solution for HackerRank Down to Zero II Problem
 *   Problem Link:- https://www.hackerrank.com/challenges/down-to-zero-ii/problem
+*   Website:- www.programmingvidya.com
 */
+
+#include <bits/stdc++.h>
+using namespace std;
 
 /*
-*   This method will return the minimum number of moves
-*   required to reduce the value of N to 0
-*/
+ * Complete the downToZero function below.
+ */
 int downToZero(int n) {
 
-    // * Initializing a queue
-    queue<pair<int,int> > q;
+    // * Initializing variables
+    queue<int> q;
+    int dp[1000001] = {0};
 
-    // * Initially, pushing n to the queue with depth as 0
-    q.push(make_pair(n, 0));
+    // * Pushing n to the queue
+    q.push(n);
 
     // * Executing the loop while queue is not empty
     while (!q.empty())
     {
-        // * Getting the current pair from queue
-        pair<int, int> cp = q.front();
+        // * Getting the current number
+        int cn = q.front();
 
-        // * Popping the current pair from queue
+        // * Popping the current number from queue
         q.pop();
 
         /*
-        *   If the value of current pair is equal to 4,
-        *   return current depth + 3
+        *   If the current number is 0, break the loop
+        *   as we don't need to scan the queue further
         */
-        if(cp.first==4) {
-            return cp.second + 3;
-        }
-        /*
-        *   If the value of current pair is less than 4,
-        *   return current depth + current value
-        */
-        else if(cp.first<4) {
-            return cp.second + cp.first;
+        if(cn==0) {
+            break;
         }
 
         /*
         *   Case 1:
-        *   Push the current value - 1 and
-        *   the current depth + 1 to queue
+        *   If the current number - 1, is not present in the queue,
+        *   Push the current number - 1 to the queue and
+        *   the number of moves + 1 to the dp array for the new value
         */
-        q.push(make_pair(cp.first - 1, cp.second + 1));
+        if(dp[cn-1]==0) {
+            q.push(cn-1);
+            dp[cn-1] = dp[cn] + 1;
+        }
 
         /*
         *   Case 2:
-        *   Start the counter with square root of current value.
+        *   Start the counter with square root of current number.
         *   While the square root is greater than or equal to 2,
-        *   decrement the counter and check if the current value is divisible
-        *   by the current value of counter. If it is, push the other factor
-        *   (which is greater than value of counter) in the queue and
-        *   increment the current depth by 1 for it's value
+        *   decrement the counter and check if the current number is divisible
+        *   by the current value of counter. If it is, and the value after division
+        *   is not already present in the queue, push the new value in the queue and
+        *   add the number of moves + 1 to the dp array for the new value
         */
-        for(int i=sqrt(cp.first); i>=2; i--) {
-            if(cp.first%i==0) {
-                q.push(make_pair(cp.first/i, cp.second + 1));
+        for(int i=sqrt(cn); i>=2; i--) {
+            if(cn%i==0 && dp[cn/i]==0) {
+                q.push(cn/i);
+                dp[cn/i] = dp[cn] + 1;
             }
         }
     }
+
+    // * Return the number of moves to reduce n to 0
+    return dp[0];
+}
+
+// * Main function as given by HackerRank
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = downToZero(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
 
     return 0;
 }
