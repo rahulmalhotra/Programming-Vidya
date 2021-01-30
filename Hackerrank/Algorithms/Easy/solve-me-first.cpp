29902740da07d001d40ddabf256b920da32cea9c
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2021-01-30 07:04:01 +0530

    Added solution for HackerRank Solve Me First problem

---
+++ Hackerrank/Algorithms/Easy/solve-me-first.cpp
@@ -0,0 +1,28 @@
+/*
+*   Author:- Rahul Malhotra
+*   Source:- Programming Vidya
+*   Description:- Solution for HackerRank Solve Me First problem
+*   Problem Link:- https://www.hackerrank.com/challenges/solve-me-first/problem
+*   Website:- www.programmingvidya.com
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  return a+b;
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
