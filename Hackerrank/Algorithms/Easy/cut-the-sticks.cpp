7528356803ea6d4fa6e68a94624c860d8b42a460
Rahul Malhotra <rahulmalhotra9620@yahoo.in>
2021-02-16 09:32:59 +0530

    Added solution for HackerRank Cut the sticks problem

---
+++ Hackerrank/Algorithms/Easy/cut-the-sticks.cpp
@@ -0,0 +1,113 @@
+/*
+*   Author:- Rahul Malhotra
+*   Source:- Programming Vidya
+*   Description:- Solution for HackerRank Cut the sticks problem
+*   Problem Link:- https://www.hackerrank.com/challenges/cut-the-sticks/problem
+*   Website:- www.programmingvidya.com
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the cutTheSticks function below.
+vector<int> cutTheSticks(vector<int> arr) {
+
+    // * Initializing variables
+    int totalCut = 0, n = arr.size();
+    vector<int> result;
+
+    // * Sorting the stick lengths
+    sort(arr.begin(), arr.end());
+
+    // * Checking each length of the sorted sticks one by one
+    for(int length : arr) {
+
+        /*
+        *   If the total cut is less than the current stick's length,
+        *   push the current number of sticks in the result vector.
+        *   Add the current stick length which is remaining
+        *   after removing the total cut encountered till now
+        *   to the total cut
+        */
+        if(totalCut<length) {
+            result.push_back(n);
+            totalCut += length - totalCut;
+        }
+
+        // * Decrementing the number of sticks by 1
+        n--;
+    }
+
+    // * Returning the result vector
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = cutTheSticks(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
